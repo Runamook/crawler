0b8493ddcb3d56463197d6fa11c2c7478b14d683
Runamook <tothe8c@gmail.com>
2018-06-04 14:09:02 +0300

    updated

--- Crawler.py
+++ Crawler.py
@@ -9,23 +9,44 @@ def get_html(url):
 
 def get_all_links(html):
     soup = BeautifulSoup(html, 'lxml')
-    
     a_s = soup.find_all('a')
     links = []
     
     for a in a_s:
         i = a.get('href')
-        if 'http' in i:
-            links.append(i)
-        
+        if 'http' in i and 'php?' not in i and len(i) > 22 :
+            links.append(i.replace(' ','').strip())
+            
     return links
 
+def get_page_data(html,link):
+    soup = BeautifulSoup(html, 'lxml')
+    
+    try:
+        #name = soup.find('span', itemprop='title').text
+        name = soup.find('a', href=link).find('span').text.strip()
+    except:
+        name = ''
+        
+    try:
+        div = soup.find('tbody').find('td').text
+    except:
+        div = ''
+        
+    data = {'name' : name,
+            'div' : div}
+    return data
+    
+    
+    
 def main():
     url = 'http://dividendo.ru/dividendnye-istorii-kompaniy.html'
     
     all_links = get_all_links(get_html(url))
     
     for link in all_links:
+        #data = get_page_data(get_html(link), link)
+        #print(link, data)
         print(link)
 
 if __name__ == '__main__':
